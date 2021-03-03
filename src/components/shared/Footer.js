539177b06be5a112505daf96aa4581882998136b
Andre <vtavares.andre@gmail.com>
2021-03-02 23:27:51 -0300

    feature: footer

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -2,11 +2,16 @@ import { Grid, Box, Typography } from "@material-ui/core"
 import React from "react"
 
 const Footer = () => {
+
+	const year = new Date().getFullYear()
 	return (
-		<Box mt={10} p={2}  bgxcolor='secondary.light'>
+		<Box mt={10} p={3} bgcolor='secondary.light'>
 			<Box component='footer'>
 
-			<Typography align="center">Developed by Andre tavares</Typography>
+				<Typography align='center'>Developed By <a href="https://www.andretavares.dev/" target="_blank">
+							Andre Tavares</a> &copy; {year}
+				</Typography>
+				
 			</Box>
 		</Box>
 	)
