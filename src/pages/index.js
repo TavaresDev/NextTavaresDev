db1f3f27f0376bc99d4c2997eb80e90eef3a03cd
Andre <vtavares.andre@gmail.com>
2021-03-05 01:04:37 -0300

    cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,22 @@
 import React from "react"
 import Hero from "../components/Hero"
 import AboutInfo from "../components/AboutInfo"
-import Color from "../components/Color"
-import AboutServices from "../components/about/AboutServices"
 import Contact from "../components/contact/Contact"
 
-import ComplexGrid from "../components/ComplexGrid"
-import Projects from "../components/projects/projects"
 import ProjectCarousel from "../components/projects/ProjectCarousel"
 import AboutServicesCarousel from "../components/about/AboutServicesCarousel"
 
 export default function Index() {
-	// const heroTitle= {<span > Tavares </span> Web Solutions }
-
 	return (
 		<>
-			{/* <Hero
+			<Hero
 				heroTitle={"web solutions"}
 				heroTitleWord={"Tavares"}
 				subTitle={"We expand your reach, while you do what "}
 				subTitleWord={"you Love "}
-			/> */}
-			{/* <ComplexGrid />
+			/>
 
-			<Color /> */}
-
-			{/* <AboutServices /> */}
-			{/* <AboutServicesCarousel/> */}
+			<AboutServicesCarousel />
 			<AboutInfo
 				order={2}
 				text1='here is some text about me company'
@@ -38,15 +28,9 @@ export default function Index() {
 				text2='here is some text about me company'
 			/>
 
-			<ProjectCarousel/>
+			<ProjectCarousel />
 			{/* <Projects/> */}
 			<Contact />
-
-			{/* <Box my={4}>
-				<Typography variant='h4' component='h1' gutterBottom>
-					Next.js example Home Page
-				</Typography>
-			</Box> */}
 		</>
 	)
 }
